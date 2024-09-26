1b508e77bc78615f67aac5b802f9ffae900f665b
Andrey <kibirev@bk.ru>
2024-09-26 19:54:23 +0300

    initial commit

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from measurement.views import SensorListCreateAPIView, MeasurementListAPIView, SensorRetrieveUpdateAPIView, \
+    MeasurementCreateAPIView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorListCreateAPIView.as_view(), name='sensor-list-create'),
+    path('sensors/<int:pk>/', SensorRetrieveUpdateAPIView.as_view(), name='sensor-detail'),
+    path('measurements/', MeasurementListAPIView.as_view(), name='measurement-list'),
+    path('measurements/create/', MeasurementCreateAPIView.as_view(), name='measurement-create'),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,25 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import generics
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer
+
+
+class SensorListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementCreateAPIView(generics.CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+class MeasurementListAPIView(generics.ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
